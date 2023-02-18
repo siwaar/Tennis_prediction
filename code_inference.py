a4d5391a11a3b44c46a0ee6b7c431d8d4a24006c
Siwar ABBES <siwar.abbes@green-acres.com>
2023-02-18 10:19:25 +0100

    inference2

--- code_inference.py
+++ code_inference.py
@@ -17,24 +17,27 @@ class ATPWinnerPredict(object):
         with open(config_path) as f:
             params = yaml.load(f)
 
-        data_test = load_data(params['data_test_csv_path'])
-        preprocessor = PreProcessing(data_test, params['features_to_drop'],  \
+        data = load_data(params['data_csv_path'])
+        
+        preprocessor = PreProcessing(data, params['features_to_drop'],  \
         params['features_to_fill_by_median'], params['features_to_remove_nan_values'])
         preprocessor.preprocess()
         preprocessed_data = preprocessor.data
+        if 'p1_won' in data.columns:
+            preprocessed_data = preprocessed_data.drop(columns=['p1_won'], axis=1)
         # Encoding
         ohe = pickle.load(open(params['onehot_encoder_path'], 'rb'))
         feature_encoder = FeaturesEncoder(params)
         # OneHotEnconder
-        encoded_oh_X_test = feature_encoder.transfrom_with_ohe(preprocessed_data, ohe)
+        encoded_oh_X = feature_encoder.transfrom_with_ohe(preprocessed_data, ohe)
         # Target Encoder
         target_encoder_params = pickle.load(open(params['target_encoder_path'], 'rb') )
-        encoded_X_test = feature_encoder.transform_with_target_encoder(encoded_oh_X_test, target_encoder_params)
-        encoded_X_test = encoded_X_test.drop(columns=['tourney_date'])
+        encoded_X = feature_encoder.transform_with_target_encoder(encoded_oh_X, target_encoder_params)
+        encoded_X = encoded_X.drop(columns=['tourney_date'])
         model =  pickle.load(open(params['model_path'], 'rb'))
-        predictions = model.predict(encoded_X_test)
+        predictions = model.predict(encoded_X)
         predictions_df = pd.DataFrame(predictions, columns=['predicted_p1_won'])
-        data_with_predictions = pd.concat([data_test, predictions_df],axis=1)
+        data_with_predictions = pd.concat([data, predictions_df],axis=1)
         data_with_predictions.to_csv(params['predictions_csv_path'])
 
 
