b3995df72f069bc3cd570a5fa355ddcd5c9b4910
Siwar ABBES <siwar.abbes@green-acres.com>
2023-02-19 19:06:06 +0100

    app

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ target_encoder_params = pickle.load(open(params['target_encoder_path'], 'rb'))
 model =  pickle.load(open(params['model_path'], 'rb'))
 # features names for prediction
 features = params['features_for_prediction']
+features_type_int = ['p1_id', 'p2_id', 'match_num', 'p1_age', 'p2_age']
+features_type_float = ['p1_rank', 'p2_rank', 'p1_rank_points', 'p2_rank_points', 'p1_seed', 'p2_seed', 'p1_ht', 'p2_ht']
+
 
 app = Flask(__name__)
 CORS(app)
@@ -62,18 +65,64 @@ def predict():
 @app.route("/atp_winner", methods=['POST','GET'])
 def predict_interface():
     # sourcery skip: for-append-to-extend, identity-comprehension, list-comprehension, move-assign-in-block, simplify-generator, use-fstring-for-formatting
-    if request.method == 'POST':
+    if request.method != 'POST':
+        return render_template('index.html')
+    try :
         values = []
         result = request.form
-        for v in result.values():
-            values.append(v)
-        proba_p1_won = predict_winner(values)
-        if proba_p1_won > 0.5 :
-            return render_template('index.html', result='The first player will succeed with probability {}%'.format( 100 * round(proba_p1_won , 2)))
-        else: 
-            return render_template('index.html', result='The second player will succeed with probability {}%'.format( 100 * round(1 - proba_p1_won , 2)))
-    return render_template('index.html')
+        for feature, value in result.items():
+            try:
+                if feature in features_type_int: value = int(value)
+                elif feature in features_type_float: value = float(value)
+                elif feature in dict_features_options and value not in dict_features_options[feature]: value = 'None'
+                elif feature == 'tourney_date' : value = value.replace('-', '')
+                values.append(value)
+            except Exception as e:
+                return render_template('index.html', result='Please verify the provided information')
 
+        proba_p1_won = predict_winner(values)
+        if proba_p1_won > 0.50:
+            return render_template(
+                    'index.html',
+                    result='Our classiffier predicted that the first player will succeed !')
+        else:
+            return render_template(
+                'index.html',
+                result='Our classiffier predicted that the second player will succeed !')
+        
+    except Exception:
+        return render_template('index.html')
+
+dict_features_options = {
+            'best_of' : ['3','5'],
+            'round' : ['R64', 'R32', 'R16', 'QF', 'R128', 'SF', 'F', 'RR', 'BR', 'Unkown'],
+            'surface' :  ['Hard', 'Grass', 'Clay', 'Carpet'],
+            'tourney_level':['M', 'A', 'G', 'D', 'F'],
+            'p1_hand' : ['R', 'L'],
+            'p2_hand': ['R', 'L'],
+            'p1_ioc' :['ESP', 'CYP', 'USA', 'NED', 'SRB', 'RUS', 'CAN', 'SVK', 'GBR',
+                                    'FRA', 'BEL', 'HUN', 'JPN', 'CRO', 'ARG', 'AUT', 'POL', 'ISR',
+                                    'SRI', 'SLO', 'IND', 'AUS', 'RSA', 'LAT', 'LUX', 'GER', 'COL',
+                                    'BOL', 'NOR', 'TPE', 'KOR', 'ITA', 'BIH', 'UKR', 'KAZ', 'TUN',
+                                    'PAK', 'CZE', 'BRA', 'EST', 'URU', 'POR', 'BAR', 'SWE', 'SUI',
+                                    'ROU', 'BUL', 'PER', 'TUR', 'LTU', 'DOM', 'GRE', 'ECU', 'UZB',
+                                    'PHI', 'CHI', 'BLR', 'THA', 'MDA', 'FIN', 'ZIM', 'VEN', 'INA',
+                                    'MON', 'CRC', 'ALG', 'CHN', 'KUW', 'DEN', 'NZL', 'EGY', 'MEX',
+                                    'GEO', 'VIE', 'PAR', 'LIB', 'MAR', 'PUR', 'ESA', 'BAH', 'MAS',
+                                    'GUA', 'IRL', 'IRI', 'MAD', 'QAT', 'HKG', 'UAE'],
+            'p2_ioc': ['ESP', 'CYP', 'USA', 'NED', 'SRB', 'RUS', 'CAN', 'SVK', 'GBR',
+                                    'FRA', 'BEL', 'HUN', 'JPN', 'CRO', 'ARG', 'AUT', 'POL', 'ISR',
+                                    'SRI', 'SLO', 'IND', 'AUS', 'RSA', 'LAT', 'LUX', 'GER', 'COL',
+                                    'BOL', 'NOR', 'TPE', 'KOR', 'ITA', 'BIH', 'UKR', 'KAZ', 'TUN',
+                                    'PAK', 'CZE', 'BRA', 'EST', 'URU', 'POR', 'BAR', 'SWE', 'SUI',
+                                    'ROU', 'BUL', 'PER', 'TUR', 'LTU', 'DOM', 'GRE', 'ECU', 'UZB',
+                                    'PHI', 'CHI', 'BLR', 'THA', 'MDA', 'FIN', 'ZIM', 'VEN', 'INA',
+                                    'MON', 'CRC', 'ALG', 'CHN', 'KUW', 'DEN', 'NZL', 'EGY', 'MEX',
+                                    'GEO', 'VIE', 'PAR', 'LIB', 'MAR', 'PUR', 'ESA', 'BAH', 'MAS',
+                                    'GUA', 'IRL', 'IRI', 'MAD', 'QAT', 'HKG', 'UAE']
+
+        }
+   
 
 # running REST interface, port=5000 for direct test
 if __name__ == "__main__":
